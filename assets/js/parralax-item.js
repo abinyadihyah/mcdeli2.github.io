b1848e5cbca6aa50c7a3528f9184b1c23c082469
abinyadihyah <54788774+abinyadihyah@users.noreply.github.com>
2022-04-08 13:15:44 +0700

    all change

--- assets/js/parralax-item.js
+++
@@ -1,9 +0,0 @@
-
-	$( document ).mousemove( function( e ) {
-
-		$( '.raw1' )	  .parallax( 30	, e );
-		$( '.raw2' )	  .parallax( 10	, e );
-		$( '.raw3' )	  .parallax( 50	, e );
-    $( '.raw4' )	  .parallax( 10	, e );
-
-	});
